7cdbac610be76117e3a117fe870b07d21cb429ce
Taylor <taylor.hocutt@gmail.com>
2023-02-04 18:25:26 -0500

    first pass setting up initial UI

--- Assets/GGJ 2023/Scripts/Runtime/UI/Views/TitleView.cs
+++ Assets/GGJ 2023/Scripts/Runtime/UI/Views/TitleView.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,6 +7,56 @@ namespace GGJRuntime
 {
     public class TitleView : GameView
     {
+        private const string STATE_KEY = "state";
 
+        private const string OPENING_KEY = "opening";
+        private const string CLOSING_KEY = "closing";
+
+        [SerializeField]
+        private Animator _buttonAnimator = null;
+        [SerializeField]
+        private float _delay = 1f;
+
+        [SerializeField]
+        private GameState _state = null;
+
+        public override void Open()
+        {
+            base.Open();
+
+            OpenSequence();
+        }
+
+        public override void Close()
+        {
+            CloseSequence();
+            _state.StateType = StateType.Gameplay;
+            StartCoroutine(DelayAction(base.Close));
+        }
+
+        public void OpenSequence()
+        {
+            _buttonAnimator.SetBool(OPENING_KEY, true);
+            _buttonAnimator.SetBool(CLOSING_KEY, false);
+        }
+
+        public void CloseSequence()
+        {
+            _buttonAnimator.SetBool(OPENING_KEY, false);
+            _buttonAnimator.SetBool(CLOSING_KEY, true);
+        }
+
+        private IEnumerator DelayAction(Action callback)
+        {
+            yield return new WaitForSeconds(_delay);
+            callback.Invoke();
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+
+            _state.StateType = StateType.Title;
+        }
     }
 }
